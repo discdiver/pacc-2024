5de130d56e595dbe509bcc75a8dd959163780c40
Jeff Hale <jeffmshale@gmail.com>
2024-02-13 14:28:32 -0500

    add ticker

--- 106/deployment-trigger.py
+++ 106/deployment-trigger.py
@@ -3,14 +3,14 @@ from prefect.events.schemas import DeploymentTrigger
 
 
 @flow(log_prints=True)
-def downstream_flow(ticker: str) -> str:
+def downstream_flow(ticker: str = "AAPL") -> str:
     print(f"got {ticker}")
 
 
 downstream_deployment_trigger = DeploymentTrigger(
     name="Upstream Flow - Sell",
     enabled=True,
-    match={"prefect.resource.id": "prefect.flow.5c933ae4-dd43-4705-90eb-cfdeb4c028fb"},
+    match={"prefect.resource.id": "5c933ae4-dd43-4705-90eb-cfdeb4c028fb"},
 )
 
 
