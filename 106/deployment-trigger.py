c3109c7010c7a4bc32e2860fee3c4f5b089c976f
Jeff Hale <jeffmshale@gmail.com>
2024-02-13 14:17:01 -0500

    update trigger to all flow runs

--- 106/deployment-trigger.py
+++ 106/deployment-trigger.py
@@ -10,7 +10,7 @@ def downstream_flow(ticker: str) -> str:
 downstream_deployment_trigger = DeploymentTrigger(
     name="Upstream Flow - Sell",
     enabled=True,
-    match={"prefect.resource.id": "prefect.flow-run.*"},
+    match={"prefect.resource.id": "prefect.flow.name.pipeline"},
 )
 
 
